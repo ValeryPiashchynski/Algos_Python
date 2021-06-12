db64019a013a01605b2d24a005e2540eacbd3bb0
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-06-13 00:05:26 +0300

    - Add leap years + tests
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- Courses/intro/nextDay.py
+++ Courses/intro/nextDay.py
@@ -1,3 +1,10 @@
+monthM = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+
+
+def monthMap(month):
+    return monthM[month]
+
+
 def nextDay(year, month, day):
     """
     Returns the year, month, day of the next day.
@@ -20,13 +27,32 @@ def nextDay(year, month, day):
     return year, month, day
 
 
+def isLeapYear(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def testIsLeap():
+    assert (isLeapYear(1992) is True)
+    assert (isLeapYear(2000) is True)
+    assert (isLeapYear(1900) is False)
+
+
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     """Returns the number of days between year1/month1/day1
        and year2/month2/day2. Assumes inputs are valid dates
        in Gregorian calendar, and the first date is not after
        the second."""
 
-    assert (dateIsBefore(year1, month1, day1, year2, month2, day2) is False)
+    # assert (dateIsBefore(year1, month1, day1, year2, month2, day2) is True)
 
     num = 0
     if year1 == year2 and month1 == month2 and day1 == day2:
@@ -52,27 +78,23 @@ def dateIsBefore(year1, month1, day1, year2, month2, day2):
     return False
 
 
-def test():
-    # assert (nextDay(1999, 12, 30) == (2000, 1, 1))
-    # assert (nextDay(2013, 1, 30) == (2013, 2, 1))
-    # assert (nextDay(2012, 1, 1) == (2012, 1, 2))
+def testDaysBetweenDates():
+    # test same day
+    assert (daysBetweenDates(2017, 12, 30,
+                             2017, 12, 30) == 0)
+    # test adjacent days
+    assert (daysBetweenDates(2017, 12, 30,
+                             2017, 12, 31) == 1)
+    # test new year
+    assert (daysBetweenDates(2017, 12, 30,
+                             2018, 1, 1) == 2)
+    # test full year difference
+    assert (daysBetweenDates(2012, 6, 29,
+                             2013, 6, 29) == 365)
 
-    test_cases = [((2012, 9, 30, 2012, 10, 30), 30),
-                  ((2012, 1, 1, 2013, 1, 1), 360),
-                  ((2012, 9, 1, 2012, 9, 4), 3)]
-
-    for (args, answer) in test_cases:
-        result = daysBetweenDates(*args)
-        if result != answer:
-            print("Test with data:", args, "failed")
-        else:
-            print("Test case passed!")
+    print("Congratulations! Your daysBetweenDates")
+    print("function is working correctly!")
 
 
-test()
-
-### For example:
-###    nextDay(1999, 12, 30) => (2000, 1, 1)
-###    nextDay(2013, 1, 30) => (2013, 2, 1)
-###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
-###
+testIsLeap()
+testDaysBetweenDates()
